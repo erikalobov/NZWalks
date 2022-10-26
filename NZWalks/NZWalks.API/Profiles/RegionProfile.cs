7fda51ef7b033ad0d33d4ad9e3a72b9d57699e8d
erikalobov <erikalobov@gmail.com>
2022-10-26 22:15:05 +0100

    get regions asynchronously implementation

---
+++ NZWalks/NZWalks.API/Profiles/RegionProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionProfile : Profile
+    {
+        public RegionProfile()
+        {
+            CreateMap<Models.DTOs.Region, Models.DTOs.Region>()
+                .ReverseMap();
+        }
+    }
+}
